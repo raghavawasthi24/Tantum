eab5e451261aa0fdbb2e383c048dc35a9b1b08a5
Raghav Awasthi <raghavawasthi240@gmail.com>
2024-01-14 10:25:26 +0530

    [home]:search input added for ride searching

--- client/src/pages/User/Home/sections/LandingPage.jsx
+++ client/src/pages/User/Home/sections/LandingPage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Input } from "../../../../components/ui/input";
+import {Button} from "../../../../components/ui/button";
+import { LuArrowUpFromDot } from "react-icons/lu";
 
 
 export default function LandingPage() {
@@ -10,7 +12,32 @@ export default function LandingPage() {
         <p className="font-medium ">Unlock Your Travel Potential: Instant Bookings, Comfortable Rides, Endless Enjoyment!</p>
       </div>
       <div className="flex">
-        <Input placeholder="From" className="w-[30rem] bg-muted outline-0 border-none rounded-none "/>
+      <div className="bg-background/95 p-2 flex flex-col gap-2 backdrop-blur supports-[backdrop-filter]:bg-background/60">   
+                <div className="grid grid-cols-2">
+                  <div className="relative">
+                    <LuArrowUpFromDot className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground text-green-700" />
+                    <Input placeholder="Search" className="pl-8 focus-visible:ring-0 rounded-none focus-visible:ring-offset-0" />
+                  </div>
+                  <div className="relative">
+                    <LuArrowUpFromDot className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+                    <Input placeholder="Search" className="pl-8 focus-visible:ring-0 rounded-none focus-visible:ring-offset-0" />
+                  </div>
+                </div>
+
+                <div className="flex">
+                  <div className="relative">
+                    <LuArrowUpFromDot className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+                    <Input placeholder="Search" className="pl-8 focus-visible:ring-0 rounded-none focus-visible:ring-offset-0" />
+                  </div>
+                  <div className="relative">
+                    <LuArrowUpFromDot className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+                    <Input placeholder="Search" className="pl-8 focus-visible:ring-0 rounded-none focus-visible:ring-offset-0" />
+                  </div>
+                  <Button size="icon" variant="outline" className="rounded-full ml-2 bg-blue-500">
+
+                  </Button>
+                </div>
+            </div>
       </div>
     </section>
   );
