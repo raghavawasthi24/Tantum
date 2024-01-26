5ce43c969dcff97a3fc21840bcf396c0193b38fa
Raghav Awasthi <raghavawasthi240@gmail.com>
2024-01-26 13:47:41 +0530

    [navbar]: refactored and added profile pic and more menu options

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,58 @@
 import { Button } from "./ui/button";
+import {Avatar, AvatarFallback, AvatarImage} from "./ui/avatar";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
 import { FaRegBell } from "react-icons/fa";
 import { BsPlusCircle } from "react-icons/bs";
 import { CiGlobe } from "react-icons/ci";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { CgProfile } from "react-icons/cg";
+import { RiMotorbikeFill } from "react-icons/ri";
+import { MdPayment } from "react-icons/md";
+import { CiSettings } from "react-icons/ci";
+import { FiLogOut } from "react-icons/fi";
+
 
 export default function Navbar() {
-  const loggedin = false;
+  const loggedin = true;
+
+  const sidebar = [
+    {
+      icon: <CgProfile className="w-4 h-4 mr-2" />,
+      title: "My Profile",
+      link: "/profile",
+    },
+    {
+      icon: <RiMotorbikeFill className="w-4 h-4 mr-2" />,
+      title: "My Rides",
+      link: "/profile",
+    },
+    {
+      icon: <MdPayment className="w-4 h-4 mr-2" />,
+      title: "My Transactions",
+      link: "/profile",
+    },
+    {
+      icon: <CiSettings className="w-4 h-4 mr-2" />,
+      title: "Settings",
+      link: "/profile",
+    },
+    {
+      icon: <FiLogOut className="w-4 h-4 mr-2" />,
+      title: "Logout",
+      link: "/profile",
+    },
+  ];
   return (
     <nav className="flex w-full fixed bg-white top-0 justify-between items-center px-4 py-2 z-20">
       <p className="font-bold text-xl md:text-4xl text-[#272142]">Tantum.</p>
@@ -31,7 +78,35 @@ export default function Navbar() {
         </Button>
         {/* checks whether user is logged in or not */}
         {loggedin ? (
-          <Button variant="secondary">hey</Button>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Avatar className="cursor-pointer">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </SheetTrigger>
+            <SheetContent className="w-[300px] pt-10">
+              {
+                // sidebar
+                sidebar.map((item, index) => (
+                  <div
+                    key={index}
+                    className="flex items-center space-x-2 text-[#272142] text-md font-semibold hover:bg-[#F3F3F3] p-2 rounded-lg cursor-pointer"
+                  >
+                    {item.icon}
+                    {item.title}
+                  </div>
+                ))
+              }
+               {/* <div className="w-full flex items-center">
+                  <CgProfile className="w-4 h-4 mr-2"/>
+                  My Profile
+               </div> */}
+            </SheetContent>
+          </Sheet>
         ) : (
           <Button
             variant="secondary"
--- client/src/pages/User/Home/sections/LandingPage.jsx
+++ client/src/pages/User/Home/sections/LandingPage.jsx
@@ -1,48 +1,60 @@
 import React from "react";
 import { Input } from "../../../../components/ui/input";
-import {Button} from "../../../../components/ui/button";
-import { LuArrowUpFromDot } from "react-icons/lu";
+import { Button } from "../../../../components/ui/button";
 import { TbLocationFilled } from "react-icons/tb";
 import { MdLocationPin } from "react-icons/md";
 import { FaCalendarAlt } from "react-icons/fa";
 import { BsPersonFill } from "react-icons/bs";
-
-
 
 export default function LandingPage() {
   return (
     <section className="mt-[56px] py-20 flex flex-col items-center clip-custom bg-ai gap-12">
       <div className="flex flex-col items-center gap-2 text-white">
         <p className="text-6xl font-bold"> Book, Ride, Enjoy!</p>
-        <p className="font-medium ">Unlock Your Travel Potential: Instant Bookings, Comfortable Rides, Endless Enjoyment!</p>
+        <p className="font-medium ">
+          Unlock Your Travel Potential: Instant Bookings, Comfortable Rides,
+          Endless Enjoyment!
+        </p>
       </div>
       <div className="flex">
-      <div className="bg-white p-1 rounded-sm bg-opacity-20 flex">   
-         
-                  <div className="relative border-r">
-                    <TbLocationFilled className="absolute left-2 top-3.5 h-4 w-4 text-muted-foreground" />
-                    <Input placeholder="Source" className="pl-8 focus-visible:ring-0 rounded-none focus-visible:ring-offset-0 h-11" />
-                  </div>
-                  <div className="relative">
-                    <MdLocationPin className="absolute left-2 top-3.5 h-4 w-4 text-muted-foreground" />
-                    <Input placeholder="Destination" className="pl-8 focus-visible:ring-0 rounded-none focus-visible:ring-offset-0 h-11" />
-                  </div>
-             
+        <div className="bg-white p-1 rounded-sm bg-opacity-20 flex">
+          <div className="relative border-r">
+            <TbLocationFilled className="absolute left-2 top-3.5 h-4 w-4 text-muted-foreground" />
+            <Input
+              placeholder="Source"
+              className="pl-8 focus-visible:ring-0 rounded-none focus-visible:ring-offset-0 h-11"
+            />
+          </div>
+          <div className="relative">
+            <MdLocationPin className="absolute left-2 top-3.5 h-4 w-4 text-muted-foreground" />
+            <Input
+              placeholder="Destination"
+              className="pl-8 focus-visible:ring-0 rounded-none focus-visible:ring-offset-0 h-11"
+            />
+          </div>
 
-               
-                  <div className="relative border-;">
-                    <FaCalendarAlt className="absolute left-2 top-3.5 h-4 w-4 text-muted-foreground" />
-                    <Input placeholder="Search" className="pl-8 focus-visible:ring-0 rounded-none focus-visible:ring-offset-0 h-11" />
-                  </div>
-                  <div className="relative">
-                    <BsPersonFill className="absolute left-2 top-3.5 h-4 w-4 text-muted-foreground" />
-                    <Input placeholder="Search" className="pl-8 focus-visible:ring-0 rounded-none focus-visible:ring-offset-0 h-11" />
-                  </div>
-                  <Button variant="outline" size="lg" className="bg-red-500 text-white border-none rounded-none h-11">
-                      Search
-                  </Button>
-              
-            </div>
+          <div className="relative border-;">
+            <FaCalendarAlt className="absolute left-2 top-3.5 h-4 w-4 text-muted-foreground" />
+            <Input
+              placeholder="Search"
+              className="pl-8 focus-visible:ring-0 rounded-none focus-visible:ring-offset-0 h-11"
+            />
+          </div>
+          <div className="relative">
+            <BsPersonFill className="absolute left-2 top-3.5 h-4 w-4 text-muted-foreground" />
+            <Input
+              placeholder="Search"
+              className="pl-8 focus-visible:ring-0 rounded-none focus-visible:ring-offset-0 h-11"
+            />
+          </div>
+          <Button
+            variant="outline"
+            size="lg"
+            className="bg-red-500 text-white border-none rounded-none h-11"
+          >
+            Search
+          </Button>
+        </div>
       </div>
     </section>
   );
